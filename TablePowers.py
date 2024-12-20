2cc164fe8b3fcb66fe81e84b67941a78560c1202
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-20 12:05:37 -0330

    function setup

--- TablePowers.py
+++ TablePowers.py
@@ -1,14 +1,21 @@
 def main():
     start_number = int(input("Start number: "))
     stop_number = int(input("Stop number: ")) + 1
+    square_number(start_number, stop_number)
+    cube_number(start_number, stop_number)
+    
+        
 
+    
+def square_number(start_number, stop_number):
     for x in range(start_number, stop_number):
         square_number =  x ** 2
         print(square_number)
-        
-
+    
+def cube_number(start_number, stop_number):
     for x in range(start_number, stop_number):   
-        cubed_number = start_number ** 3
+        cubed_number = x ** 3
         print(cubed_number)
     
+
 main()
