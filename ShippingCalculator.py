09a9c75561f8c538b3bae129d9b26482d429d1c3
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-20 11:24:27 -0330

    Logic corrected , rounded two decimals,returns top of loop if negative is entered

--- ShippingCalculator.py
+++ ShippingCalculator.py
@@ -1,18 +1,21 @@
 def main():
-    cost_of_items = float(input("Cost of items ordered"))
+ while True:
+    cost_of_items = float(input("Cost of items ordered: "))
     if cost_of_items >= 75.00:
         shipping_cost = 0
     elif cost_of_items < 75.00 and cost_of_items > 50 :
         shipping_cost = 9.95
     elif cost_of_items < 50 and cost_of_items > 30:
         shipping_cost = 7.95
-    elif cost_of_items < 30:
+    elif cost_of_items < 30 and cost_of_items > 0:
         shipping_cost = 5.95
-    else:
-        print("Number must be greater than 0")
+    elif cost_of_items < 0:
+        print("Must enter a positive number")
+        continue
+
+    total_cost = cost_of_items + shipping_cost
     
-    
-    
-    print(f"Cost of items ordered: ")
-    print(f"Shipping cost: ")
-    print(f"Total cost: ")
+    print(f"Cost of items ordered: {cost_of_items:.2f}")
+    print(f"Shipping cost: {shipping_cost:.2f}")
+    print(f"Total cost: {total_cost:.2f}")
+main()
